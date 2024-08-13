129f12ca52262a7f7f408990079aa156aa0d2f53
Prashanth S N <163889724+Prashanth0718@users.noreply.github.com>
2024-08-13 11:23:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const quoteText = document.querySelector(".quote");
+const authorName = document.querySelector(".name");
+const quoteBtn = document.querySelector("button");
+const soundBtn = document.querySelector(".sound");
+const copyBtn = document.querySelector(".copy");
+const twitterBtn = document.querySelector(".twitter");
